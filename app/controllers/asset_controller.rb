611fe634eb0f5f81fb764214482fdb0002e755b4
Stephen-Harding <StephenHarding79@gmail.com>
2017-12-19 18:11:58 -0500

    fixed relative paths

--- app/controllers/asset_controller.rb
+++ app/controllers/asset_controller.rb
@@ -1,20 +1,20 @@
 class AssetController < ApplicationController
   def map
-  	render file: "/Users/stephenharding/Desktop/WDI/Unit_04/GameApp/assets/games/collision_test4.json"
+    render file: "./app/assets/game/collision_test4.json"
   end
   def tiles
-  	send_file "/Users/stephenharding/Desktop/WDI/Unit_04/GameApp/assets/games/tiles2.png", type: 'image/png', disposition: 'inline'
+    send_file "./app/assets/game/tiles2.png", type: 'image/png', disposition: 'inline'
   end
   def ground
-  	send_file "/Users/stephenharding/Desktop/WDI/Unit_04/GameApp/assets/games/ground_1x1.png", type: 'image/png', disposition: 'inline'
+    send_file "./app/assets/game/ground_1x1.png", type: 'image/png', disposition: 'inline'
   end
    def walls
-  	send_file "/Users/stephenharding/Desktop/WDI/Unit_04/GameApp/assets/games/walls_1x2.png", type: 'image/png', disposition: 'inline'
+    send_file "./app/assets/game/walls_1x2.png", type: 'image/png', disposition: 'inline'
   end
     def dude
-  	send_file "/Users/stephenharding/Desktop/WDI/Unit_04/GameApp/assets/games/dude189x35.png", type: 'image/png', disposition: 'inline'
+    send_file "./app/assets/game/dude189x35.png", type: 'image/png', disposition: 'inline'
   end
    def kidney
-  	send_file "/Users/stephenharding/Desktop/WDI/Unit_04/GameApp/assets/games/Kidney.png", type: 'image/png', disposition: 'inline'
+    send_file "./app/assets/game/Kidney.png", type: 'image/png', disposition: 'inline'
   end
 end
